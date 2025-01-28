a8dad2981e74204325224f938f1fbbb461b7d32c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2025-01-28 14:02:19 +0100

    started a probleme a little bit confused gotta redo it

---
+++ longestCommonPrefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        short = min(strs, key=len)
+        for i in short:
+            for string in strs:
+                if string[i] != strs
+        
