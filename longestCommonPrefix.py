e827c7915bb7aa7b6325d8824cc950a7b6e33d27
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2025-01-28 14:02:50 +0100

    fix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -9,7 +9,7 @@ class Solution(object):
         if len(strs) == 1:
             return strs[0]
         short = min(strs, key=len)
-        for i in short:
+        """for i in short:
             for string in strs:
-                if string[i] != strs
+                if string[i] != strs:"""
         
